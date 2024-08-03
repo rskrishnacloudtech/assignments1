83f8a0cea51112edb6635f4ea1b6de255141b509
Siva <rsk.sfdctraining1@gmail.com>
2024-08-03 10:45:44 +0530

    Assignments

--- 03.cpu-usage-alert.sh
+++ 03.cpu-usage-alert.sh
@@ -1,17 +1,17 @@
 #!/bin/bash 
 
 # Getting the top 5 CPU usage.
-cpuusage=$(ps aux | sort -nrk 3,3 | head -n 5) 
+cpuusage=$(ps aux | sort -nrk 3,3 | head -n 5 | awk -F " " '{print $4f}') 
 
 # Getting the process name from the result.
-processname=$(ps aux | sort -nrk 3,3 | head -n 5 | awk -F " " '{print $1f}') 
+processname=$(ps aux | sort -nrk 3,3 | head -n 5 | awk -F " " '{print $1f}' | awk -F " " '{print $1f}') 
 threshold=10 
 
 # Reading all the records line by line and and cheking the usage is greater than threshold limit set and priting the process name which is more.
 while IFS= read -r line 
 do 
-    usage=$($line | awk -F " " '{print $4f}')
-    processname=$($line | awk -F " " '{print $1f}')
+    # usage=$(echo $line | awk -F " " '{print $4f}')
+    # processname=$($line | awk -F " " '{print $1f}')
     if [ $usage -gt $threshold ]
     then
         echo "$processname is consuming more CPU memory than $threshold. Current uage is $usage" 
