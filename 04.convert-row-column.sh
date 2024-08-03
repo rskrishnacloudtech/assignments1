96db5b3d8dca26db7c415e998bf6aef921c8dff3
Siva <rsk.sfdctraining1@gmail.com>
2024-08-03 11:20:11 +0530

    Assignments

---
+++ 04.convert-row-column.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <input_file> <output_file>"
+  exit 1
+fi
+
+input_file=$1
+output_file=$2
+
+# Transpose the input file using awk
+awk '
+{
+  for (i=1; i<=NF; i++)  {
+    matrix[NR,i] = $i
+  }
+  max_cols = (NF > max_cols ? NF : max_cols)
+  max_rows = NR
+}
+END {
+  for (i=1; i<=max_cols; i++) {
+    for (j=1; j<=max_rows; j++) {
+      printf("%s%s", matrix[j,i], (j==max_rows ? "" : " "))
+    }
+    print ""
+  }
+}
+' "$input_file" > "$output_file"
+
+echo "Transposition complete. Output written to $output_file"
