00a643d8737a161460cec4ade860a4b778eb8908
Siva <rsk.sfdctraining1@gmail.com>
2024-08-03 11:16:11 +0530

    Assignments

--- 03.cpu-usage-alert.sh
+++ 03.cpu-usage-alert.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Set the CPU usage threshold
-THRESHOLD=5
+THRESHOLD=80
+
+# Email address to send alert
+EMAIL="your_email@example.com"
 
 # Get the top 5 CPU consuming processes
 TOP_PROCESSES=$(ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -n 6)
@@ -16,5 +19,6 @@ ALERT=$(echo "$TOP_PROCESSES" | awk -v threshold=$THRESHOLD 'NR>1 && $5+0 > thre
 if [ -n "$ALERT" ]; then
     echo "ALERT: Some processes are consuming more than $THRESHOLD% CPU:"
     echo "$ALERT"
-    # You can add additional actions here, like sending an email or notification
+    # Send email alert. We should have configure the mail server.
+    echo "$ALERT" | mail -s "CPU Usage Alert" "$EMAIL"
 fi
