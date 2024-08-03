6747509d79591f6375965f14503859afbfad6153
Siva <rsk.sfdctraining1@gmail.com>
2024-08-03 08:44:18 +0530

    Assignments

---
+++ 02.tar-folder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Forming a time stamp
+timestamp=$(date +%F-%H-%M-%S)
+fromFolder=/home/ec2-user/fromfolder/
+toFolder=/home/ec2-user/tofolder/
+tarFileName="file-backup-$timestamp.ziptar.gz"
+
+tar cvzf "$toFolder/$tarFileName" $fromFolder
+
