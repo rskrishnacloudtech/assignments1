d6aea1c21d2a49326d0d7c02e6c321a835fada03
Siva <rsk.sfdctraining1@gmail.com>
2024-08-03 08:45:03 +0530

    Assignments

--- 02.tar-folder.sh
+++ 02.tar-folder.sh
@@ -4,7 +4,7 @@
 timestamp=$(date +%F-%H-%M-%S)
 fromFolder=/home/ec2-user/fromfolder/
 toFolder=/home/ec2-user/tofolder/
-tarFileName="file-backup-$timestamp.ziptar.gz"
+tarFileName="file-backup-$timestamp.tar.gz"
 
 tar cvzf "$toFolder/$tarFileName" $fromFolder
 
